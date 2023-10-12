e858c0dfc6a766386c4092c0bb68922374ed44f1
Khrystyna Kuts <kuts.k@ucu.edu.ua>
2023-10-12 23:31:16 +0300

    linter fix attempt no 8

--- src/main/java/flower/filters/FlowerFilter.java
+++ src/main/java/flower/filters/FlowerFilter.java
@@ -1,24 +1,28 @@
+/**
+ * This package contains utility classes for filtering flower search.
+ */
 package flower.filters;
 
 import java.util.List;
 
 import flower.item.FlowerBucket;
+import flower.item.FlowerPack;
 import flower.item.Item;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class FlowerFilter implements SearchFilter {
 
-    /** minimum price of bucket */
+    /** minimum price of bucket. */
     private static final double MIN_PRICE = 0;
-    /** maximum price of bucket */ 
+    /** maximum price of bucket. */
     private static final double MAX_PRICE = 5000;
-    /** minimum number of flowers in bucket */ 
+    /** minimum number of flowers in bucket. */
     private static final int MIN_NUM_FLOWERS = 0;
-    /** maximum number of flower in bucket */
+    /** maximum number of flower in bucket. */
     private static final int MAX_NUM_FLOWERS = 1000;
 
-    /** match item with price and number of flowers */
+    /** match item with price and number of flowers. */
     @Override
     public final boolean match(Item item) {
 
@@ -35,7 +39,7 @@ public class FlowerFilter implements SearchFilter {
             price = true;
         }
 
-        List<?> flowerPack = bucket.getflowerPacks();
+        List<FlowerPack> flowerPack = bucket.getflowerPacks();
 
         if ((flowerPack.size() >= MIN_NUM_FLOWERS)
                 && (flowerPack.size() <= MAX_NUM_FLOWERS)) {
